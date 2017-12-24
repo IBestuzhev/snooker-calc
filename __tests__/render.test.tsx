15123c2f6d7e86e5e7cdf367a47da399d92525e4
Igor Bestuzhev <best.igor@gmail.com>
2017-12-24 20:41:26 +0200

    Test of connected component

--- __tests__/render.test.tsx
+++ __tests__/render.test.tsx
@@ -1,9 +1,11 @@
-import { PotsList } from "../src/components/scores";
+import { PotsList } from "../src/components/scores.potlist";
 import { StatePot } from "../src/reducers/scores";
 import { mount } from 'enzyme';
 import * as React from "react";
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 
 Enzyme.configure({ adapter: new (Adapter as () => void)() });
@@ -29,8 +31,20 @@ describe("Render Scores", () => {
                 isFreeball: false,
                 isFaul: false
             },
+            {
+                player: "left",
+                score: 1,
+                isFreeball: false,
+                isFaul: false
+            },
         ];
-        let PotWrapper = mount(<PotsList pots={pots} />)
-        expect(PotWrapper.find("Chip").length).toBe(2);
+        let defaultStoreState = {
+            score: {
+                pots: pots
+            }
+        }
+        let store = createStore((s = defaultStoreState) => s, defaultStoreState)
+        let PotWrapper = mount(<Provider store={store}><PotsList player="left" popupOpener={() => {}} /></Provider>)
+        expect(PotWrapper.find("Chip").length).toBe(3);  // One for every type and +1 for last pot
     })
 })
